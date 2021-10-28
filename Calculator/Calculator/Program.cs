ed2349821e4ac0159c0ff804a34255a707fde3ab
beerappa <beerappadandaragi@gmail.com>
2021-10-28 12:56:36 +0530

    addtion of 2 number

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class cal
+    {
+        int totalvalue;
+        public int sumis(int num1,int num2)
+        {
+            return totalvalue= num1 + num2;
+        }
+        static void Main(string[] args)
+        {
+            cal objcal = new cal();
+            Console.WriteLine("total values is:" + objcal.sumis(52,38));
+            Console.ReadKey();
+        }
+    }
+}
