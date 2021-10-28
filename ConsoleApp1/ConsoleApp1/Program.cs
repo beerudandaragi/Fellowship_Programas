31a07b1642fdf3b4ff58bdbfd29193e0e5924462
beerappa <beerappadandaragi@gmail.com>
2021-10-28 06:44:37 +0530

    helloword

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class claculator
+    {
+       int totalvalue;
+       public int issum(int num1,int num2)
+    {
+        return totalvalue = num1 + num2;
+    }
+
+        static void Main(string[] args)
+    {
+        claculator claculator2 = new claculator();
+        Console.WriteLine("total values:" + claculator2.issum(30, 20));
+        Console.ReadKey();
+    }
+        
+           
+        }
+    }
+
